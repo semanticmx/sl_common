11e27fe19e3b2b797832ab29bb94f16616048bb6
Carlos Vences <carlos@semantic.mx>
2024-10-30 21:45:00 -0600

    Fix tests import

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
 
-
 setup(
     name='sl_common',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(where="src", exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    package_dir={"": "src"},
     install_requires=[
-        # Add any external dependencies here
     ],
 )
